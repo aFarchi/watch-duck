759ff6bc61e9e0ff09fc5fe74e2d281f87917ea8
Alban Farchi <alban.farchi@enpc.fr>
2026-04-08 16:47:13 +0200

    simplified progress functions

--- src/watch_duck/parse.py
+++ src/watch_duck/parse.py
@@ -56,117 +56,77 @@ def check_frequencies(freq_00, freq_12, freq):
         raise ValueError(message)
 
 
+def current_date_to_index(progress):
+    date_start = progress['date_start']
+    date_freq = progress['date_freq']
+    return {
+        key: value
+        if not key.startswith('current_')
+        else (value - date_start) // date_freq
+        for key, value in progress.items()
+    }
+
+
 def get_progress_fc_1_experiment(node_ini, node_fc, node_lag, group):
     check_group(group)
-    node_ini_00 = node_ini['children'][group]
-    node_fc_00 = node_fc['children'][group]
-    node_lag_00 = node_lag['children'][group]
     step = int(group) * pd.Timedelta('1h')
-    date_start = pd.Timestamp(node_ini_00['ymd_start']) + step
-    date_end = pd.Timestamp(node_ini_00['ymd_end']) + step
-    date_freq = int(node_ini_00['ymd_freq']) * pd.Timedelta('1D')
-    date_current_ini = pd.Timestamp(node_ini_00['ymd_current']) + step
-    date_current_fc = pd.Timestamp(node_fc_00['ymd_current']) + step
-    date_current_lag = pd.Timestamp(node_lag_00['ymd_current']) + step
-    index_ini = (date_current_ini - date_start) // date_freq
-    index_fc = (date_current_fc - date_start) // date_freq
-    index_lag = (date_current_lag - date_start) // date_freq
-    if node_ini_00['state'] == 'complete':
-        index_ini += 1
-        node_ini_00['state'] = 'queued'
-    if node_fc_00['state'] == 'complete':
-        index_fc += 1
-        node_fc_00['state'] = 'queued'
-    if node_lag_00['state'] == 'complete':
-        index_lag += 1
-        node_lag_00['state'] = 'queued'
+    date_start = pd.Timestamp(node_ini['children'][group]['ymd_start']) + step
+    date_end = pd.Timestamp(node_ini['children'][group]['ymd_end']) + step
+    date_freq = int(node_ini['children'][group]['ymd_freq']) * pd.Timedelta('1D')
+    date_ini = pd.Timestamp(node_ini['children'][group]['ymd_current']) + step
+    date_fc = pd.Timestamp(node_fc['children'][group]['ymd_current']) + step
+    date_lag = pd.Timestamp(node_lag['children'][group]['ymd_current']) + step
+    if node_ini['children'][group]['state'] == 'complete':
+        date_ini += date_freq
+        node_ini['children'][group]['state'] = 'queued'
+    if node_fc['children'][group]['state'] == 'complete':
+        date_fc += date_freq
+        node_fc['children'][group]['state'] = 'queued'
+    if node_lag['children'][group]['state'] == 'complete':
+        date_lag += date_freq
+        node_lag['children'][group]['state'] = 'queued'
     return {
         'date_start': date_start,
         'date_end': date_end,
         'date_freq': date_freq,
-        'index_ini': index_ini,
-        'index_fc': index_fc,
-        'index_lag': index_lag,
-        'state_ini': encode_state(node_ini_00['state']),
-        'state_fc': encode_state(node_fc_00['state']),
-        'state_lag': encode_state(node_lag_00['state']),
+        'current_ini': date_ini,
+        'current_fc': date_fc,
+        'current_lag': date_lag,
+        'state_ini': encode_state(node_ini['children'][group]['state']),
+        'state_fc': encode_state(node_fc['children'][group]['state']),
+        'state_lag': encode_state(node_lag['children'][group]['state']),
     }
 
 
 def get_progress_fc_2_experiment(node_ini, node_fc, node_lag, ini_groups):
     check_groups(ini_groups)
-    node_ini_00 = node_ini['children']['00']
-    node_fc_00 = node_fc['children']['00']
-    node_lag_00 = node_lag['children']['00']
-    node_ini_12 = node_ini['children']['12']
-    node_fc_12 = node_fc['children']['12']
-    node_lag_12 = node_lag['children']['12']
-    ini_00_state = node_ini_00['state']
-    fc_00_state = node_fc_00['state']
-    lag_00_state = node_lag_00['state']
-    ini_12_state = node_ini_12['state']
-    fc_12_state = node_fc_12['state']
-    lag_12_state = node_lag_12['state']
-    step_00 = pd.Timedelta('0h')
-    step_12 = pd.Timedelta('12h')
-    date_start_00 = pd.Timestamp(node_ini_00['ymd_start']) + step_00
-    date_end_00 = pd.Timestamp(node_ini_00['ymd_end']) + step_00
-    date_freq_00 = int(node_ini_00['ymd_freq']) * pd.Timedelta('1D')
-    date_current_ini_00 = pd.Timestamp(node_ini_00['ymd_current']) + step_00
-    date_current_fc_00 = pd.Timestamp(node_fc_00['ymd_current']) + step_00
-    date_current_lag_00 = pd.Timestamp(node_lag_00['ymd_current']) + step_00
-    if ini_00_state == 'complete':
-        date_current_ini_00 += date_freq_00
-        ini_00_state = 'queued'
-    if fc_00_state == 'complete':
-        date_current_fc_00 += date_freq_00
-        fc_00_state = 'queued'
-    if lag_00_state == 'complete':
-        date_current_lag_00 += date_freq_00
-        lag_00_state = 'queued'
-    date_start_12 = pd.Timestamp(node_ini_12['ymd_start']) + step_12
-    date_end_12 = pd.Timestamp(node_ini_12['ymd_end']) + step_12
-    date_freq_12 = int(node_ini_12['ymd_freq']) * pd.Timedelta('1D')
-    date_current_ini_12 = pd.Timestamp(node_ini_12['ymd_current']) + step_12
-    date_current_fc_12 = pd.Timestamp(node_fc_12['ymd_current']) + step_12
-    date_current_lag_12 = pd.Timestamp(node_lag_12['ymd_current']) + step_12
-    if ini_12_state == 'complete':
-        date_current_ini_12 += date_freq_12
-        ini_12_state = 'queued'
-    if fc_12_state == 'complete':
-        date_current_fc_12 += date_freq_12
-        fc_12_state = 'queued'
-    if lag_12_state == 'complete':
-        date_current_lag_12 += date_freq_12
-        lag_12_state = 'queued'
-    if date_start_00 < date_start_12:
-        date_start = date_start_00
-        date_end = date_end_12
-        date_freq = date_start_12 - date_start_00
+    progress_00 = get_progress_fc_1_experiment(node_ini, node_fc, node_lag, '00')
+    progress_12 = get_progress_fc_1_experiment(node_ini, node_fc, node_lag, '12')
+    if progress_00['date_start'] < progress_12['date_start']:
+        date_start = progress_00['date_start']
+        date_end = progress_12['date_end']
+        date_freq = progress_12['date_start'] - progress_00['date_start']
     else:
-        date_start = date_start_12
-        date_end = date_end_00
-        date_freq = date_start_00 - date_start_12
-    check_frequencies(date_freq_00, date_freq_12, date_freq)
-    date_current_ini = min(date_current_ini_00, date_current_ini_12)
-    date_current_fc = min(date_current_fc_00, date_current_fc_12)
-    date_current_lag = min(date_current_lag_00, date_current_lag_12)
-    index_ini = (date_current_ini - date_start) // date_freq
-    index_fc = (date_current_fc - date_start) // date_freq
-    index_lag = (date_current_lag - date_start) // date_freq
+        date_start = progress_12['date_start']
+        date_end = progress_00['date_end']
+        date_freq = progress_00['date_start'] - progress_12['date_start']
+    check_frequencies(progress_00['date_freq'], progress_12['date_freq'], date_freq)
+    date_ini = min(progress_00['current_ini'], progress_12['current_ini'])
+    date_fc = min(progress_00['current_fc'], progress_12['current_fc'])
+    date_lag = min(progress_00['current_lag'], progress_12['current_lag'])
     return {
         'date_start': date_start,
         'date_end': date_end,
         'date_freq': date_freq,
-        'index_ini': index_ini,
-        'index_fc': index_fc,
-        'index_lag': index_lag,
-        'state_ini_00': encode_state(ini_00_state),
-        'state_fc_00': encode_state(fc_00_state),
-        'state_lag_00': encode_state(lag_00_state),
-        'state_ini_12': encode_state(ini_12_state),
-        'state_fc_12': encode_state(fc_12_state),
-        'state_lag_12': encode_state(lag_12_state),
+        'current_ini': date_ini,
+        'current_fc': date_fc,
+        'current_lag': date_lag,
+        'state_ini_00': encode_state(node_ini['children']['00']['state']),
+        'state_fc_00': encode_state(node_fc['children']['00']['state']),
+        'state_lag_00': encode_state(node_lag['children']['00']['state']),
+        'state_ini_12': encode_state(node_ini['children']['12']['state']),
+        'state_fc_12': encode_state(node_fc['children']['12']['state']),
+        'state_lag_12': encode_state(node_lag['children']['12']['state']),
     }
 
 
@@ -239,118 +199,76 @@ def check_an_experiment(experiment):
 
 def get_progress_an_1_experiment(node_obs, node_main, node_lag, kind, group):
     check_group(group, kind=kind)
-    node_obs_00 = node_obs['children'][group]
-    node_main_00 = node_main['children'][group]
-    node_lag_00 = node_lag['children'][group]
-    obs_00_state = node_obs_00['state']
-    main_00_state = node_main_00['state']
-    lag_00_state = node_lag_00['state']
     step = int(group[len(kind) :]) * pd.Timedelta('1h')
-    date_start = pd.Timestamp(node_obs_00['ymd_start']) + step
-    date_end = pd.Timestamp(node_obs_00['ymd_end']) + step
-    date_freq = int(node_obs_00['ymd_freq']) * pd.Timedelta('1D')
-    date_current_obs = pd.Timestamp(node_obs_00['ymd_current']) + step
-    date_current_main = pd.Timestamp(node_main_00['ymd_current']) + step
-    date_current_lag = pd.Timestamp(node_lag_00['ymd_current']) + step
-    if obs_00_state == 'complete':
-        date_current_obs += date_freq
-        obs_00_state = 'queued'
-    if main_00_state == 'complete':
-        date_current_main += date_freq
-        main_00_state = 'queued'
-    if lag_00_state == 'complete':
-        date_current_lag += date_freq
-        lag_00_state = 'queued'
-    index_obs = (date_current_obs - date_start) // date_freq
-    index_main = (date_current_main - date_start) // date_freq
-    index_lag = (date_current_lag - date_start) // date_freq
+    date_start = pd.Timestamp(node_obs['children'][group]['ymd_start']) + step
+    date_end = pd.Timestamp(node_obs['children'][group]['ymd_end']) + step
+    date_freq = int(node_obs['children'][group]['ymd_freq']) * pd.Timedelta('1D')
+    date_obs = pd.Timestamp(node_obs['children'][group]['ymd_current']) + step
+    date_main = pd.Timestamp(node_main['children'][group]['ymd_current']) + step
+    date_lag = pd.Timestamp(node_lag['children'][group]['ymd_current']) + step
+    if node_obs['children'][group]['state'] == 'complete':
+        date_obs += date_freq
+        node_obs['children'][group]['state'] = 'queued'
+    if node_main['children'][group]['state'] == 'complete':
+        date_main += date_freq
+        node_main['children'][group]['state'] = 'queued'
+    if node_lag['children'][group]['state'] == 'complete':
+        date_lag += date_freq
+        node_lag['children'][group]['state'] = 'queued'
     return {
         'date_start': date_start,
         'date_end': date_end,
         'date_freq': date_freq,
-        'index_obs': index_obs,
-        'index_main': index_main,
-        'index_lag': index_lag,
-        'state_obs': encode_state(obs_00_state),
-        'state_main': encode_state(main_00_state),
-        'state_lag': encode_state(lag_00_state),
+        'current_obs': date_obs,
+        'current_main': date_main,
+        'current_lag': date_lag,
+        'state_obs': encode_state(node_obs['children'][group]['state']),
+        'state_main': encode_state(node_main['children'][group]['state']),
+        'state_lag': encode_state(node_lag['children'][group]['state']),
     }
 
 
 def get_progress_an_2_experiment(node_obs, node_main, node_lag, kind, obs_groups):
     check_groups(obs_groups, kind=kind)
-    node_obs_00 = node_obs['children'][f'{kind}00']
-    node_main_00 = node_main['children'][f'{kind}00']
-    node_lag_00 = node_lag['children'][f'{kind}00']
-    node_obs_12 = node_obs['children'][f'{kind}12']
-    node_main_12 = node_main['children'][f'{kind}12']
-    node_lag_12 = node_lag['children'][f'{kind}12']
-    obs_00_state = node_obs_00['state']
-    main_00_state = node_main_00['state']
-    lag_00_state = node_lag_00['state']
-    obs_12_state = node_obs_12['state']
-    main_12_state = node_main_12['state']
-    lag_12_state = node_lag_12['state']
-    step_00 = pd.Timedelta('0h')
-    step_12 = pd.Timedelta('12h')
-    date_start_00 = pd.Timestamp(node_obs_00['ymd_start']) + step_00
-    date_end_00 = pd.Timestamp(node_obs_00['ymd_end']) + step_00
-    date_freq_00 = int(node_obs_00['ymd_freq']) * pd.Timedelta('1D')
-    date_current_obs_00 = pd.Timestamp(node_obs_00['ymd_current']) + step_00
-    date_current_main_00 = pd.Timestamp(node_main_00['ymd_current']) + step_00
-    date_current_lag_00 = pd.Timestamp(node_lag_00['ymd_current']) + step_00
-    if obs_00_state == 'complete':
-        date_current_obs_00 += date_freq_00
-        obs_00_state = 'queued'
-    if main_00_state == 'complete':
-        date_current_main_00 += date_freq_00
-        main_00_state = 'queued'
-    if lag_00_state == 'complete':
-        date_current_lag_00 += date_freq_00
-        lag_00_state = 'queued'
-    date_start_12 = pd.Timestamp(node_obs_12['ymd_start']) + step_12
-    date_end_12 = pd.Timestamp(node_obs_12['ymd_end']) + step_12
-    date_freq_12 = int(node_obs_12['ymd_freq']) * pd.Timedelta('1D')
-    date_current_obs_12 = pd.Timestamp(node_obs_12['ymd_current']) + step_12
-    date_current_main_12 = pd.Timestamp(node_main_12['ymd_current']) + step_12
-    date_current_lag_12 = pd.Timestamp(node_lag_12['ymd_current']) + step_12
-    if obs_12_state == 'complete':
-        date_current_obs_12 += date_freq_12
-        obs_12_state = 'queued'
-    if main_12_state == 'complete':
-        date_current_main_12 += date_freq_12
-        main_12_state = 'queued'
-    if lag_12_state == 'complete':
-        date_current_lag_12 += date_freq_12
-        lag_12_state = 'queued'
-    if date_start_00 < date_start_12:
-        date_start = date_start_00
-        date_end = date_end_12
-        date_freq = date_start_12 - date_start_00
+    progress_00 = get_progress_an_1_experiment(
+        node_obs,
+        node_main,
+        node_lag,
+        kind,
+        f'{kind}00',
+    )
+    progress_12 = get_progress_an_1_experiment(
+        node_obs,
+        node_main,
+        node_lag,
+        kind,
+        f'{kind}12',
+    )
+    if progress_00['date_start'] < progress_12['date_start']:
+        date_start = progress_00['date_start']
+        date_end = progress_12['date_end']
+        date_freq = progress_12['date_start'] - progress_00['date_start']
     else:
-        date_start = date_start_12
-        date_end = date_end_00
-        date_freq = date_start_00 - date_start_12
-    check_frequencies(date_freq_00, date_freq_12, date_freq)
-    date_current_obs = min(date_current_obs_00, date_current_obs_12)
-    date_current_main = min(date_current_main_00, date_current_main_12)
-    date_current_lag = min(date_current_lag_00, date_current_lag_12)
-    index_obs = (date_current_obs - date_start) // date_freq
-    index_main = (date_current_main - date_start) // date_freq
-    index_lag = (date_current_lag - date_start) // date_freq
+        date_start = progress_12['date_start']
+        date_end = progress_00['date_end']
+        date_freq = progress_00['date_start'] - progress_12['date_start']
+    check_frequencies(progress_00['date_freq'], progress_12['date_freq'], date_freq)
+    date_obs = min(progress_00['current_obs'], progress_12['current_obs'])
+    date_main = min(progress_00['current_main'], progress_12['current_main'])
+    date_lag = min(progress_00['current_lag'], progress_12['current_lag'])
     return {
         'date_start': date_start,
         'date_end': date_end,
         'date_freq': date_freq,
-        'index_obs': index_obs,
-        'index_main': index_main,
-        'index_lag': index_lag,
-        'state_obs_00': encode_state(obs_00_state),
-        'state_main_00': encode_state(main_00_state),
-        'state_lag_00': encode_state(lag_00_state),
-        'state_obs_12': encode_state(obs_12_state),
-        'state_main_12': encode_state(main_12_state),
-        'state_lag_12': encode_state(lag_12_state),
+        'current_obs': date_obs,
+        'current_main': date_main,
+        'current_lag': date_lag,
+        'state_obs_00': encode_state(node_obs['children'][f'{kind}00']['state']),
+        'state_main_00': encode_state(node_main['children'][f'{kind}00']['state']),
+        'state_lag_00': encode_state(node_lag['children'][f'{kind}00']['state']),
+        'state_obs_12': encode_state(node_obs['children'][f'{kind}12']['state']),
+        'state_main_12': encode_state(node_main['children'][f'{kind}12']['state']),
+        'state_lag_12': encode_state(node_lag['children'][f'{kind}12']['state']),
     }
 
 
@@ -390,9 +308,10 @@ def get_progress_experiment(experiment):
     experiment_type = get_experiment_type(experiment)
     match experiment_type:
         case 'fc':
-            return get_progress_fc_experiment(experiment)
+            progress = get_progress_fc_experiment(experiment)
         case 'lw' | 'elda':
-            return get_progress_an_experiment(experiment, kind=experiment_type)
+            progress = get_progress_an_experiment(experiment, kind=experiment_type)
+    return current_date_to_index(progress)
 
 
 def parse_family_line(line):
@@ -519,9 +438,10 @@ def save_experiment_progress(wdir, name, experiment, date, chunk_size=128):
         coords={
             'time': (('time',), [date]),
         },
-        attrs={key: value for key, value in progress.items() if 'date' in key}.update({
-            'name': name,
-        }),
+        attrs={
+            key: progress[key].strftime('%Y-%m-%dT%H:%M:%S')
+            for key in ('date_start', 'date_end')
+        }|{'date_freq': int(progress['date_freq'].total_seconds()) // 3600},
     )
     # chunking in time
     for key in progress:
@@ -559,11 +479,11 @@ def parse_log_files(wdir):
 
         path_active = wdir / f'active/{suite["name"]}.txt'
         path_active.parent.mkdir(parents=True, exist_ok=True)
-        logger.info('saving active experiment list into %s', path_active)
+        logger.info('saving active experiment list into "%s"', path_active)
         with pathlib.Path(path_active).open('w', encoding=None) as f:
             for name in suite['experiments']:
                 f.write(f'{name}\n')
 
         new_name = path_log_arxiv / log_file.name
-        logger.info('renaming log file into %s', new_name)
+        logger.info('renaming log file into "%s"', new_name)
         log_file.rename(new_name)
